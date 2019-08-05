cec94784fac0d12f3eafd6857d143572f3721573
kchhak <kane.chhak@gmail.com>
2019-08-05 18:19:07 -0400

    Complete WordChainer class

--- word_chainer.rb
+++ word_chainer.rb
@@ -1,5 +1,7 @@
 class WordChainer
-    attr_reader :dictionary
+    attr_reader :dictionary, :all_seen_words
+    attr_accessor :current_words
+
     def initialize(dictionary_file_name)
         @dictionary = File.readlines(dictionary_file_name, chomp: true).to_set
     end
@@ -17,5 +19,36 @@ class WordChainer
             end
         end
         new_words
+    end
+
+    def run(source, target)
+        @current_words = [source]
+        @all_seen_words = {source => nil}
+
+        explore_current_words until current_words.empty? || all_seen_words.include?(target)
+        build_path(target)
+    end
+    
+    def explore_current_words
+        new_current_words = []
+        current_words.each do |current_word|
+            adjacent_words(current_word).each do |adjacent_word|
+                next if all_seen_words.include?(adjacent_word)
+                new_current_words << adjacent_word
+                all_seen_words[adjacent_word] = current_word
+            end
+        end
+        self.current_words = new_current_words
+        #new_current_words.each{|new_word| puts new_word + " <- " + all_seen_words[new_word]}
+    end
+
+    def build_path(target)
+        path = []
+        current_word = target
+        until current_word.nil?
+            path << current_word
+            current_word = @all_seen_words[current_word]
+        end
+        path.reverse
     end
 end
