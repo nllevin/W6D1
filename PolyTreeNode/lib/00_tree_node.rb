8162e39bf7182fc1a84188adaa1b5c22ab73427d
nllevin <50681582+nllevin@users.noreply.github.com>
2019-08-05 13:59:27 -0400

    Implement PolyTreeNode#initialize, getters, setters

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,15 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
 
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(new_parent)
+        parent.children.delete(self) unless parent.nil?
+        @parent = new_parent
+        new_parent.children << self unless new_parent.nil?
+    end
 end
