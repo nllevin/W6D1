edc3435d60a1b90ef72ca69f86b12a98152ee608
nllevin <50681582+nllevin@users.noreply.github.com>
2019-08-05 14:04:01 -0400

    Add PolyTreeNode#add_child, #remove_child methods

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -12,4 +12,13 @@ class PolyTreeNode
         @parent = new_parent
         new_parent.children << self unless new_parent.nil?
     end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "That is not my child" unless children.include?(child_node)
+        child_node.parent = nil
+    end
 end
